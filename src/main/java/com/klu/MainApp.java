63f37309df0381da4cb24f87f8d26da05ae75b44
Bhargavi <bhargavi123@gmail.com>
2026-01-02 08:15:48 +0530

    Bug fix changes

--- src/main/java/com/klu/MainApp.java
+++ src/main/java/com/klu/MainApp.java
@@ -2,7 +2,7 @@ package com.klu;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("Git Maven Project");
+        System.out.println("Hello world!");
         System.out.println("Bug fixed");
     }
 }
